b3aec7234e1e10c2670115d174d1c872b07fc54a
David Wang <david.wang@kirinou.top>
2021-04-22 12:23:21 +0000

    Add Index and old index

--- ProjectSaki/views.py
+++ ProjectSaki/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
  
 def index(request):
-    context          = {} # 新建字典
-    context['title'] = "Index"  # 添加键值
-    return render(request, 'index.html', context)
+    # 针对模板标签初始化字典
+    body = {} 
+    # 模板具体内容
+    body["head_title"] = "首页"
+    body["body_title"] = "图像评分" 
+    return render(request, template_name="index.html", context=body)
+
+def index_old(request):
+    return render(request, template_name="index_old.html")
